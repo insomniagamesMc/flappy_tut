9a98e3d118750701d0fed7aa1f1e91c2393fb94f
Perbon <insomniagamestrue@gmail.com>
2024-05-23 20:30:02 -0600

    add pipe

--- code/flappy.js
+++ code/flappy.js
@@ -9,6 +9,16 @@ let birdWidth = 34;
 let birdHeight = 24;
 let birdX = boardWidth/8;
 let birdY = boardHeight/2;
+let birdImg;
+
+let pipeArray = [];
+let pipeWidth = 64;
+let pipeHeight = 512;
+let pipeX = boarderWidth;
+let pipeY = 0;
+
+let topPipeImg;
+let bottomPipeImg;
 
 let bird = {
     x : birdX,
@@ -24,6 +34,28 @@ window.onload = function () {
     context = board.getContext("2d");
 
     //draws bird
-    context.fillStyle = "green"
-    context.fillRect(bird.x, bird.y, bird.width, bird.height)
+    //context.fillStyle = "green";
+    //context.fillRect(bird.x, bird.y, bird.width, bird.height);
+
+
+    birdImg = new Image();
+    birdImg.src = "/textures/flappybird.png";
+    birdImg.onload = function() {
+        context.drawImage(birdImg, bird.x, bird.y, bird.width, bird.height);
+    }
+
+    topPipeImg = new Image();
+    topPipeImg.src = "/textures/toppipr.png";
+
+    bottomPipeImg = new Image();
+    bottomPipeImg.src = "/textures/bottompipe.png";
+
+    requestAnimationFrame(update);
+}
+
+function upadate(){
+    requestAnimationFrame(update);
+    context.clearRect(0, 0, board.width, board.height);
+
+    context.drawImage(birdImg, bird.x, bird.y, bird.width, bird.height)
 }
